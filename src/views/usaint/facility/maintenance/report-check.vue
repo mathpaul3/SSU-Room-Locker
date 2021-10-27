994df4c7e9900ebe25e27be42723d4456f7849f1
mathpaul3 <mathpaul3@gmail.com>
2021-10-28 04:27:45 +0900

    modify text

--- src/views/usaint/facility/maintenance/report-check.vue
+++ src/views/usaint/facility/maintenance/report-check.vue
@@ -1,5 +1,9 @@
 <template>
-  <div id="no_content">This Page will not be embodied!</div>
+  <div id="no_content">
+    This Page will not be embodied!<br /><br /><br />
+    Please Go to 시설사용 > 장소사용신청 and enjoy your<br /><br />
+    사물함 사용신청 / 강의실 사용신청 / 운동장 사용신청!
+  </div>
 </template>
 
 <style>
